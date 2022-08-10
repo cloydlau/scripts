832437bada3162bb1a69d99e3ffc5f7d87f43dd8
cloydlau <253055734@qq.com>
2022-08-11 01:15:41 +0800

    style: 略

--- src/clean.ts
+++ src/clean.ts
@@ -3,5 +3,5 @@ import run from '../utils/run.ts'
 export default async (options) => {
   console.log(`%cRemoving ./node_modules`, 'color:red;font-weight:bold')
   await run('rd /s /q .\\node_modules')
-  console.log(`%c./node_modules is removed\n`, 'color:green;font-weight:bold')
+  console.log(`%cDone!\n`, 'color:green;font-weight:bold')
 }
