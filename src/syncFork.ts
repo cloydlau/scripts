eeaaebc8700212d05b082580a07e39203004c834
cloydlau <253055734@qq.com>
2022-08-15 15:42:55 +0800

    chore: 略

--- src/syncFork.ts
+++ src/syncFork.ts
@@ -16,7 +16,7 @@ export default async (options) => {
       await run('git merge up/dev', { cwd })
       console.log(`\n%c${cwd} is up-to-date with origin\n`, 'color:green;font-weight:bold')
     } catch (e) {
-      console.error(`\nSync ${cwd} failed`)
+      console.log(`\n%cSync ${cwd} failed`, 'color:red;font-weight:bold')
       console.error(e)
     }
   }
