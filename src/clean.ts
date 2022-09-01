4b4e88ea727f57bb980503e20a873a7043d0756c
cloydlau <253055734@qq.com>
2022-09-01 23:12:04 +0800

    chore(clean): show time cost

--- src/clean.ts
+++ src/clean.ts
@@ -2,6 +2,8 @@ import run from './utils/run.ts'
 
 export default async (options) => {
   console.log(`%cRemoving ./node_modules...`, 'color:red;font-weight:bold')
+  const startTime = performance.now()
   await run('rd /s /q .\\node_modules')
-  console.log(`%cDone!\n`, 'color:green;font-weight:bold')
+  const cost = ((performance.now() - startTime) / 1000).toFixed(2)
+  console.log(`%cDone in ${cost}s.\n`, 'color:green;font-weight:bold')
 }
