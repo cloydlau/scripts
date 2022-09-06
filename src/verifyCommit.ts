d1fd910d72e0006cec601bfdfe8321492e8f7d88
cloydlau <253055734@qq.com>
2022-09-06 17:31:51 +0800

    feat(verifyCommit): 支持 breaking change 标识

--- src/verifyCommit.ts
+++ src/verifyCommit.ts
@@ -2,7 +2,7 @@ import chalk from "https://deno.land/x/chalk_deno@v4.1.1-deno/source/index.js"
 
 export default () => {
   const msg = Deno.readTextFileSync('.git/COMMIT_EDITMSG').trim()
-  const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+  const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?!?: .{1,50}/
 
   if (!commitRE.test(msg)) {
     console.error(
