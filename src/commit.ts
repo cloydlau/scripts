edf86500b13466ee956e79dcc737213c295bb43d
cloydlau <253055734@qq.com>
2022-09-09 21:24:25 +0800

    feat(commit)!: modify 'subject' option

--- src/commit.ts
+++ src/commit.ts
@@ -1,16 +1,16 @@
 import run from './utils/run.ts'
 
-export default async (type, { subject = 'polish' }) => {
+export default async (type: string, subject = 'polish') => {
   console.log('\nCommitting changes...')
-  await run('git add -A')
+  await run(['git add -A'])
 
   // working tree clean 会报错
   try {
-    await run(null, { cmd: ['git', 'commit', '-m', `${type}: ${subject}`] })
+    await run(['git', 'commit', '-m', `${type}: ${subject}`])
 
     console.log('\nPushing...')
-    await run(`git push`)
-  } catch (error) {
-
+    await run(['git push'])
+  } catch (e) {
+    console.error(`\n%c${e}`, 'color:red;font-weight:bold')
   }
 }
