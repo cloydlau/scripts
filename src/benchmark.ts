65e43c9fa2470d143fe22bb7112af90ea9b54407
cloydlau <253055734@qq.com>
2022-09-09 21:23:08 +0800

    feat(benchmark)!: quetos no longer needed

--- src/benchmark.ts
+++ src/benchmark.ts
@@ -1,6 +1,6 @@
 import run from './utils/run.ts'
 
-export default async (cmd) => {
+export default async (cmd: string[]) => {
   const startTime = performance.now()
   await run(cmd)
   const cost = ((performance.now() - startTime) / 1000).toFixed(2)
