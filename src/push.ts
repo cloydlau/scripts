b5b2d411be3605ea365a3390780e92994547b154
cloydlau <253055734@qq.com>
2022-09-09 23:51:58 +0800

    chore(push)!: rename 'cl commit' to 'cl push'

--- src/push.ts
+++ src/push.ts
@@ -1,10 +1,10 @@
 import run from './utils/run.ts'
 
-export default async (type: string, subject = 'polish') => {
+export default async (type: string, subject = ['polish']) => {
   console.log('\nCommitting changes...')
   await run(['git add -A'])
   // working tree clean 会报错
-  await run(['git', 'commit', '-m', `${type}: ${subject}`])
+  await run(['git', 'commit', '-m', `${type}: ${subject.join(' ')}`])
   console.log('\nPushing...')
   await run(['git push'])
 }
