3712407a199cb490acd7274f1b634c8daac4d7d8
cloydlau <253055734@qq.com>
2022-09-17 15:17:38 +0800

    fix: pathspec 'xxx' did not match any file(s) known to git

--- src/push.ts
+++ src/push.ts
@@ -1,11 +1,11 @@
 import run from './utils/run.ts'
 
 export default async (type: string, subject = ['polish']) => {
-  const m = `${type}: ${subject.join(' ')}`
-  console.log(`\n%cCommitting "${m}"...`, 'color:#409EFF; font-weight:bold;')
+  const m = `"${type}: ${subject.join(' ')}"`
+  console.log(`\n%cCommitting ${m}...`, 'color:#409EFF; font-weight:bold;')
   await run(['git add -A'])
   // working tree clean 会报错
-  await run(['git', 'commit', '-m', `"${m}"`])
+  await run(['git', 'commit', '-m', m])
   console.log('\n%cPushing...', 'color:#409EFF; font-weight:bold;')
   await run(['git push'])
 }
