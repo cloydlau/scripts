88107ed7811e5b2af2df126cb8952bbce0567d57
cloydlau <253055734@qq.com>
2023-01-04 22:31:28 +0800

    fix(up): pkg manager version in package.json could still be stale when pkg manager is not updated

--- src/utils/updatePackageManager.ts
+++ src/utils/updatePackageManager.ts
@@ -1,13 +1,14 @@
 import run from './run.ts'
 
-export default async (packageManager: string, pkg?: object): Promise<boolean> => {
+export default async (packageManager: string, pkg?: { packageManager?: string }): Promise<boolean> => {
   console.log(`\n%cChecking ${packageManager} version...`, 'color:#409EFF; font-weight:bold;')
   const currentVersion = await run({ cmd: [`${packageManager} -v`], stdout: 'piped' })
   const latestVersion = await run({ cmd: [`npm view ${packageManager} version`], stdout: 'piped' })
 
+  let isPackageManagerVersionInPackageJSONUpdated = false
+
   if (currentVersion === latestVersion) {
     console.log(`%c${packageManager} is up-to-date`, 'color:grey; font-weight:bold;')
-    return false
   } else {
     console.log(`%cUpdating ${packageManager} version from ${currentVersion} to ${latestVersion}...`, 'color:red; font-weight:bold;')
     const registry = await run({ cmd: [`${packageManager} config get registry`], stdout: 'piped' })
@@ -19,22 +20,21 @@ export default async (packageManager: string, pkg?: object): Promise<boolean> =>
 
     console.log(`\nRecovering store-dir to ${storeDir}...`)
     await run([`${packageManager} config set store-dir ${storeDir}`])
-
-    let isPackageManagerVersionInPackageJSONUpdated = false
-    try {
-      pkg ??= JSON.parse(Deno.readTextFileSync('./package.json'))
-
-      if (pkg.packageManager) {
-        const [currentPackageManager, currentVersion] = pkg.packageManager.split('@')
-        if (currentPackageManager === packageManager && currentVersion !== latestVersion) {
-          pkg.packageManager = `${currentPackageManager}@${latestVersion}`
-          isPackageManagerVersionInPackageJSONUpdated = true
-        }
-      }
-    } catch (_e) {
-      // fix(deno lint): Empty block statement
-    }
-
-    return isPackageManagerVersionInPackageJSONUpdated
   }
+
+  try {
+    pkg ??= JSON.parse(Deno.readTextFileSync('./package.json'))
+
+    if (pkg?.packageManager) {
+      const [currentPackageManager, currentVersion] = pkg.packageManager.split('@')
+      if (currentPackageManager === packageManager && currentVersion !== latestVersion) {
+        pkg.packageManager = `${currentPackageManager}@${latestVersion}`
+        isPackageManagerVersionInPackageJSONUpdated = true
+      }
+    }
+  } catch (_e) {
+    // fix(deno lint): Empty block statement
+  }
+
+  return isPackageManagerVersionInPackageJSONUpdated
 }
